61e60bd826bb6c90c4c4dbe4f6f9dc697a6aab50
hagleyson <hagleyson@gmail.com>
2021-12-31 00:52:06 -0300

    feat: add modal error

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Adduser from "./Users/AddUser";
+import UsersList from "./Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const addUserhandler = (uName, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [...prevUsersList, { name: uName, age: uAge }];
+    });
+  };
   return (
     <div>
-      <Adduser />
+      <Adduser onAddUser={addUserhandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
